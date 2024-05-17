a006553d9265062024d4c5e2a5a0438b3a1e2dcf
Wonders Victor <wondersprince1@gmail.com>
2024-05-17 19:43:39 +0000

    Testing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -11,7 +11,42 @@ possible status code: 200, 301, 400, 401, 403, 404, 405 and 500
 if a status code doesn’t appear or is not an integer, don’t print anything for this status code
 format: <status code>: <number>
 status codes should be printed in ascending order'''
+import re
 import sys
+from collections import Counter
 
+
+status_code_dict = {
+        '200': 0,
+        '301': 0,
+        '400': 0,
+        '401': 0,
+        '403': 0,
+        '404': 0,
+        '405': 0,
+        '500': 0
+        }
+total_filesize = 0
+count = 0
+result = []
 for line in sys.stdin:
-    print("You have printed {}".format(line))
+    pattern = r"^(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}) - (\[\d{4}-\d{1,2}-\d{1,2} \d{2}:\d{2}:\d{2}\.\d{6}\]) (\"GET \/projects\/260 HTTP\/1.1\" \w* \w*)$"
+    if re.match(pattern, line):
+        count += 1
+        split_content = line.split()
+        status_code = split_content[-2]
+        file_size = split_content[8] if len(split_content) > 8 else None
+        #print("You have printed {} {}".format(status_code, file_size))
+        result.append(status_code)
+        total_filesize += int(file_size) if type(int(file_size)) is int else 0
+        if count == 10:
+            print("File size:",total_filesize)
+            details = Counter(result)
+            sorted_keys = sorted(details.keys())
+            for key in sorted_keys:
+                status_code_dict[key] += details[key]
+            for key, value in status_code_dict.items():
+                if value > 0:
+                    print(f"{key}: {value}")
+            count = 0
+            result = []
