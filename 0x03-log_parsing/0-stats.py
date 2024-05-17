86be468b8ad7a14239048bafdd5fb6ebf3385d97
Wonders Victor <Wondersprince1@gmail.com>
2024-05-17 21:02:57 +0100

    Corrected shebang

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 '''reads stdin line by line and computes metrics:
 
 Input format: <IP Address> - [<date>] "GET /projects/260 HTTP/1.1" <status code> <file size> (if the format is not this one, the line must be skipped)
