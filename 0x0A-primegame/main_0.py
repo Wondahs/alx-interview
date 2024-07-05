79e0dbdf1231a116a74ebd3a106a06e959596d11
Wonders Victor <wondersprince1@gmail.com>
2024-07-05 06:04:15 +0000

    Test

--- 0x0A-primegame/main_0.py
+++ 0x0A-primegame/main_0.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
+"""
+Main file for testing
+"""
 
 isWinner = __import__('0-prime_game').isWinner
 
+nums = [0] * 10000
+for i in range(10000):
+    nums[i] = i
 
-print("Winner: {}".format(isWinner(7, [29, 500, 103, 466, 3, 2900, 83])))
+print("Winner: {}".format(isWinner(10000, nums)))
